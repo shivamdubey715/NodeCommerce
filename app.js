b1c5f16ab1b00bfbd1efaedff2ff87747fc48609
shivamdubey715 <81400047+shivamdubey715@users.noreply.github.com>
2023-09-18 23:12:47 +0530

    new commits

--- app.js
+++ app.js
@@ -1,39 +1,50 @@
 const express = require("express");
+const fs = require("fs");
+const https = require("https");
 const path = require("path");
 const User = require("./models/user");
 const mongoose = require("mongoose");
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 const errorController = require("./controllers/404");
-const csrf = require('csurf');
-const flash = require('connect-flash');
-const multer = require('multer');
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const multer = require("multer");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+require("dotenv").config();
 
 const bodyParser = require("body-parser");
 
-const MONGODB_URI = "mongodb+srv://shivammessi07:a6Vh2kZZG9lWCNqJ@cluster0.gzsvjbu.mongodb.net/shop"
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.gzsvjbu.mongodb.net/${process.env.MONGO_DATABASE}`;
+
+// const MONGODB_URI = "mongodb+srv://shivammessi07:a6Vh2kZZG9lWCNqJ@cluster0.gzsvjbu.mongodb.net/shop"
 
 const app = express();
 
 const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: "sessions"
+  collection: "sessions",
 });
 
 const csrfProtection = csrf();
+
+const privateKey = fs.readFileSync("server.key");
+const certificate = fs.readFileSync("server.cert");
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'images');
+    cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
-  }
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
 });
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
   ) {
     cb(null, true);
   } else {
@@ -47,17 +58,25 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
+const { Stream } = require("stream");
 
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // this is to add the css file to html because the method which we know dont work
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "images")));
-app.use(session({secret: "my_secret", resave: false, saveUninitialized: false, store: store }));
-app.use(csrfProtection); 
+app.use(
+  session({
+    secret: "my_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
@@ -71,24 +90,32 @@ app.use((req, res, next) => {
     return next();
   }
   User.findById(req.session.user._id)
-    .then(user => {
-      if(!user){
+    .then((user) => {
+      if (!user) {
         return next();
       }
       req.user = user;
       next();
     })
-    .catch(err => {
+    .catch((err) => {
       next(new Error(err));
     });
 });
-
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
-app.get('/500', errorController.get500);
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan("combined", { stream: accessLogStream }));
+
+app.get("/500", errorController.get500);
 
 app.use(errorController.get404);
 
@@ -96,19 +123,20 @@ app.use((error, req, res, next) => {
   console.log(error);
   // res.status(error.httpStatusCode).render(...);
   // res.redirect('/500');
-  res.status(500).render('500', {
-    pageTitle: 'Error!',
-    path: '/500',
-    isAuthenticated: req.session.isLoggedIn
+  res.status(500).render("500", {
+    pageTitle: "Error!",
+    path: "/500",
+    isAuthenticated: req.session.isLoggedIn,
   });
 });
 
 mongoose
   .connect(MONGODB_URI)
-  .then(result => {
-    app.listen(4000);
+  .then((result) => { 
+  //   https
+  //     .createServer({ key: privateKey, cert: certificate }, app)
+      app.listen(process.env.PORT || 4000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
- 
